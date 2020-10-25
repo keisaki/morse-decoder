0d221640b88e960392afaed1ec09d8fded09d2ec
Дмитрий Криволапов <dimakrivolapov@gmail.com>
2020-10-25 14:35:50 +0400

    feat: final Solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,57 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const Simple_Alphabet = {
+    '.':     '10',
+    '-':   '11',
+};
+
 function decode(expr) {
-    // write your solution here
+
+    let words  = expr.split('**********');
+    let splittedWords = [];
+    let characters = [];
+    words.forEach(element => {
+
+        let word = [];
+        while(element.length>=10)
+        {
+
+            let character = element.slice(element.length-10);
+            element = element.substring(0,element.length-10);
+            word.push(character);
+        }
+        if(element!= '')
+            word.push(element);
+
+        splittedWords.push(word);
+    });
+    let morseString = '';
+    splittedWords.forEach(word=>{
+        word.forEach(symbol=>{
+            morseString+=(symbol.replace(/11/g,'-').replace(/10/g,'.'));
+            morseString+=' '
+        })
+        morseString+= '   ';
+    })
+    
+    morseString = morseString.replace(/00/g,'');
+
+    
+    let semFinal = [];
+
+    morseString.split('   ').forEach(el=>semFinal.push(el.split(' ').reverse().join(' ')));
+
+    let finalString = '';
+    
+    semFinal.forEach(element => {
+        element.split(' ').forEach(el=>{
+            if(el!='')
+                finalString+=MORSE_TABLE[el];
+        })
+        finalString+=' ';
+    });
+return finalString.replace(/\s+/g, ' ').trim();
 }
 
 module.exports = {
